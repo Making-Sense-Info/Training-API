06d3109104d6a1e9b77224f89bb53f0058de3a0e
Nicolas Laval <nicolas.laval@making-sense.info>
2024-12-15 16:45:20 +0100

    Remove swapi adherence

--- src/main/java/info/makingsense/training/api/service/DataService.java
+++ src/main/java/info/makingsense/training/api/service/DataService.java
@@ -1,42 +1,53 @@
 package info.makingsense.training.api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import info.makingsense.training.api.model.Person;
 import info.makingsense.training.api.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class DataService {
 
-    private static final String SWAPI_PEOPLE_URL = "https://swapi.dev/api/people/";
+    private static final String LOCAL_JSON_FILE = "data.json";
 
     @Autowired
     private PersonRepository personRepository;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     public void importData() {
-        // Récupérer les données depuis SWAPI
-        Map<String, Object> response = restTemplate.getForObject(SWAPI_PEOPLE_URL, Map.class);
-        List<Map<String, String>> people = (List<Map<String, String>>) response.get("results");
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        for (Map<String, String> personData : people) {
-            Person person = new Person();
-            person.setName(personData.get("name"));
-            person.setHeight(personData.get("height"));
-            person.setMass(personData.get("mass"));
-            person.setHairColor(personData.get("hair_color"));
-            person.setSkinColor(personData.get("skin_color"));
-            person.setEyeColor(personData.get("eye_color"));
-            person.setBirthYear(personData.get("birth_year"));
-            person.setGender(personData.get("gender"));
-            personRepository.save(person);
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(LOCAL_JSON_FILE)) {
+            if (inputStream == null) {
+                throw new IOException("Could not find file: " + LOCAL_JSON_FILE);
+            }
+
+            // Parse the JSON file as a List
+            List<Map<String, String>> people = objectMapper.readValue(inputStream, List.class);
+
+            // Process and save each person
+            for (Map<String, String> personData : people) {
+                Person person = new Person();
+                person.setName(personData.get("name"));
+                person.setHeight(personData.get("height"));
+                person.setMass(personData.get("mass"));
+                person.setHairColor(personData.get("hair_color"));
+                person.setSkinColor(personData.get("skin_color"));
+                person.setEyeColor(personData.get("eye_color"));
+                person.setBirthYear(personData.get("birth_year"));
+                person.setGender(personData.get("gender"));
+                personRepository.save(person);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
 }
 
